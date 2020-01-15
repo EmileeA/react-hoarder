2eb2f0d0d0c04119932e91d263341567dbcbc8a7
Emilee Mitchell <emcakes@gmail.com>
2020-01-15 17:43:41 -0600

    Reading My Stuff

--- src/components/pages/MyStuff/MyStuff.js
+++ src/components/pages/MyStuff/MyStuff.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import authData from '../../../helpers/data/authData';
+import itemsData from '../../../helpers/data/itemsData';
+import Stuff from '../../shared/Stuff/Stuff';
 
 import './MyStuff.scss';
 
 class MyStuff extends React.Component {
+  state = {
+    stuff: [],
+  }
+
+  getStuff = () => {
+    itemsData.getItemsByUid(authData.getUid())
+      .then((stuff) => this.setState({ stuff }))
+      .catch((error) => console.error('error from my stuff', error));
+  }
+
+  componentDidMount() {
+    this.getStuff();
+  }
+
   render() {
     return (
       <div className="MyStuff">
       <h1>MyStuff</h1>
+      <div className="stuff container d-flex flex-wrap">
+        {this.state.stuff.map((item) => <Stuff key={item.id} item={item} />)}
+      </div>
       </div>
     );
   }
